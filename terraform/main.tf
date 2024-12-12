d209943ca36d04c6b1ea2b34f68d830e81ea9dda
ThevinduKevin <thevindukevin@gmail.com>
2024-12-12 20:18:33 +0530

    commit root main file

---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+terraform {
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~> 5.0"
+        }
+    }
+    backend "s3" {
+        bucket = "ML-terraform-state"
+        key    = "ML-terraform.tfstate"
+        region = "ap-south-1"
+        dynamodb_table = "ML-terraform-state-lock"
+    }
+}
+
+provider "aws" {
+    region = var.aws_region
+} 
+
+module "s3" {
+    source = "./modules/s3" 
+}
+
+module "ec2" {
+    source = "./modules/ec2"
+    image_url = module.ecr.image_url
+}
+
+module "ecr" {
+    source = "./modules/ecr"
+}
