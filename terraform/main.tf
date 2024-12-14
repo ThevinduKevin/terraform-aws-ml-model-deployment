b2eb1c16a21af4177a49c4f63e87a990b9de580d
ThevinduKevin <thevindukevin@gmail.com>
2024-12-14 16:39:37 +0530

    commit essential files

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,12 +5,7 @@ terraform {
         version = "~> 5.0"
         }
     }
-    backend "s3" {
-        bucket = "ml-terraform-state-bucket-thev"
-        key    = "ml-terraform.tfstate"
-        region = "ap-south-1"
-        dynamodb_table = "ml-terraform-state-lock"
-    }
+    
 }
 
 provider "aws" {
