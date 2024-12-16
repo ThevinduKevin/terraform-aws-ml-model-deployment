243cd86cf1d210f422363101bf0dc48e524049f8
ThevinduKevin <thevindukevin@gmail.com>
2024-12-16 23:22:13 +0530

    changes commit

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "ml-terraform-state-bucket-thev"
-    key    = "terraform.tfstate"
+    key    = "ml-terraform-state-bucket-thev/ml-terraform.tfstate/terraform.tfstate"
     region = "ap-south-1"
     dynamodb_table = "ml-terraform-state-lock"
     encrypt = true
--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,7 @@ terraform {
 
 provider "aws" {
     region = var.aws_region
+    profile = "ml-terraform"
 } 
 
 module "s3" {
