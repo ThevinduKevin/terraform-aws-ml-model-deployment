946b510e109316d54c600c02bdf54c1c3b08b534
ThevinduKevin <thevindukevin@gmail.com>
2024-12-16 21:36:50 +0530

    changes

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "app_server" {
+/*resource "aws_instance" "app_server" {
   ami = "ami-0a8bc3e7e4bf8c115"
   instance_type = "t2.micro"
 
@@ -16,4 +16,25 @@ resource "aws_instance" "app_server" {
 
 output "public_ip" {
     value = aws_instance.app_server.public_ip
+}*/
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0a8bc3e7e4bf8c115"
+  instance_type = "t2.micro"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo yum install -y docker
+              sudo service docker start
+              sudo docker run -d -p 80:5000 ${var.image_url}
+              EOF
+
+  tags = {
+    Name = "ml-app-server"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.app_server.public_ip
 }
--- terraform/modules/ec2/variables.tf
+++ terraform/modules/ec2/variables.tf
@@ -1,5 +1,5 @@
 variable "image_url" {
     description = "The URL of the Docker image to deploy on the EC2 instance"
     type = string
-  
+    default = "docker.io/thevindukevin/ml-app:tag"
 }
