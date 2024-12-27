31f5884310885681508f2519f21f79e07cd0aacf
ThevinduKevin <thevindukevin@gmail.com>
2024-12-27 11:40:40 +0530

    latest changes

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "ml-terraform-state-bucket-thev"
-    key    = "ml-terraform-state-bucket-thev/ml-terraform.tfstate/terraform.tfstate"
-    region = "ap-south-1"
+    bucket         = "ml-terraform-state-bucket-thev"
+    key            = "ml-terraform.tfstate/terraform.tfstate"
+    region         = "ap-south-1"
     dynamodb_table = "ml-terraform-state-lock"
-    encrypt = true
+    encrypt        = true
   }
 }
